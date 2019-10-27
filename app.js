cb81ae5a87eb6317c66b99b34357d2ea0e6477e4
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-10-27 18:03:02 +0530

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const Twit = require("twit");
+const data = require("./tweets");
+
+const T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
